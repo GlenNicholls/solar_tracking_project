209c661a7a7e206b368435760faf2ce4866970db
Glen Nicholls <dkillers303@gmail.com>
2018-09-13 19:34:32 -0600

    just for the sake of committing...

--- src/software/raspberry_pi/src/setup.py
+++ src/software/raspberry_pi/src/setup.py
@@ -1,10 +1,14 @@
 import sys
 import os
 
-def LoadSearchPaths():
+def load_search_paths():
     print('-I- configuring system environment...')
     for i,j,y in os.walk(os.getcwd()):
         if(str(i).find('__pycache__') == -1 and str(i).find('.vscode') == -1):
             sys.path.append(i)
     print('-I- the following have been added to the system path:')
     print(sys.path)
+
+
+if __name__ == "__main__":
+    load_search_paths()
