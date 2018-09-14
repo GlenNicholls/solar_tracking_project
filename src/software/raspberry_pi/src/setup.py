245769de113442533eead74b6c7167ff33840b09
Glen Nicholls <dkillers303@gmail.com>
2018-09-13 20:07:30 -0600

    added todo item for path as I didn't realize this method doesn't add to  path

--- src/software/raspberry_pi/src/setup.py
+++ src/software/raspberry_pi/src/setup.py
@@ -1,14 +1,15 @@
 import sys
 import os
 
+# todo: expand function to add to $PYTHONPATH if it is not already there...
+# having a long list like 'import module.package.something' is really inconvenient
 def load_search_paths():
     print('-I- configuring system environment...')
     print('-I- the following have been added to the system path:')
     for i,j,y in os.walk(os.getcwd()):
         if(str(i).find('__pycache__') == -1 and str(i).find('.vscode') == -1):
             sys.path.append(i)
-            print(sys.path[i])
-    #print(sys.path)
+            print(i)
 
 
 if __name__ == "__main__":
