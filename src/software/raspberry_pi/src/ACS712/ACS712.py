ac914e103c25e28f70fc8965e1a89fe88f12d16a
Glen Nicholls <dkillers303@gmail.com>
2018-09-15 00:13:03 -0600

    updates for ACS712 package to fix some minor issues, next will be working on a DS3231 package along with high-level status/ctrl, logging, database, voltage sensor, etc.

--- src/software/raspberry_pi/src/ACS712/ACS712.py
+++ src/software/raspberry_pi/src/ACS712/ACS712.py
@@ -13,36 +13,44 @@ class ACS712(object):
     # _XX    : device current rating
     # A      : current unit [Amps]
     # U | B  : unidirectional or bidirectional
-    def __init__(self, device_type='ACS712_05AB',
-                       vref=3.3, 
-                       ac_freq=60,
-                       num_adc_bits=10, 
-                       adc_port=0,
-                       num_cal_avgs=20,
-                       hardware_spi=True, 
+    def __init__(self, device_type  = 'ACS712_05AB',
+                       vref         = 3.3, 
+                       ac_freq      = 60,
+                       num_adc_bits = 10, 
+                       adc_channel  = 0,
+                       num_cal_avgs = 20,
+                       hardware_spi = True, 
                        ):
-        if device_type.lower.find('ab') > -1: # if bidirectional current sensot, our zero is ADC_resolution/2
+
+        self._ADC_res = 2.0**num_adc_bits
+
+        # set zero point based on device
+        if device_type.lower().find('ab') > -1: # if bidirectional current sensot, our zero is ADC_resolution/2
             self._zero = self._ADC_res/2
-        elif device_type.lower.find('au') > -1: # if unidirectional, zero is 0
+        elif device_type.lower().find('au') > -1: # if unidirectional, zero is 0
             self._zero = 0
         else:
-            print('-E- you have not specified a valid device current sensing polarity, reference documentation')
-            print('-E- valid inputs are: ACS712_05AB, ACS712_05AU, ACS712_20AB, ACS712_20AU, ACS712_30AB, or ACS712_30AU')
             self._zero = None
 
-        if device_type.lower == 'acs712_05au' or device_type.lower == 'acs712_05ab':
+        # set device sensitivity
+        if device_type.lower() == 'acs712_05au' or device_type.lower() == 'acs712_05ab':
             self._sensitivity = 0.185
-        elif device_type.lower == 'acs712_20au' or device_type.lower == 'acs712_20ab':
+        elif device_type.lower() == 'acs712_20au' or device_type.lower() == 'acs712_20ab':
             self._sensitivity = 0.100
-        elif device_type.lower == 'acs712_30au' or device_type.lower == 'acs712_30ab':
+        elif device_type.lower() == 'acs712_30au' or device_type.lower() == 'acs712_30ab':
             self._sensitivity = 0.066
         else:
-            print('-E- you have not specified a valid device current sensing polarity, reference documentation')
-            print('-E- valid inputs are: ACS712_05AB, ACS712_05AU, ACS712_20AB, ACS712_20AU, ACS712_30AB, or ACS712_30AU')
             self._sensitivity = None
+
+        # ensure valid enteries from user
+        if self._sensitivity == None or self._zero == None:
+            print('-E- Invalid current sensing device! You entered: {}'.format(device_type))
+            print('-E- Valid inputs are: ACS712_05AB, ACS712_05AU, ACS712_20AB, ACS712_20AU, ACS712_30AB, or ACS712_30AU')
+            return
 
         # todo: abstract this at some point to give dynamic control at top level
         # todo: abstract this procedure to use any ADC by grabbing functions/objects passed in
+        # determine if we are using HW or SW SPI
         if hardware_spi:
             self.SPI_PORT   = 0
             self.SPI_DEVICE = 0
@@ -53,40 +61,44 @@ class ACS712(object):
             MOSI = 24
             CS   = 25
             self._mcp = ADC.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
-
+        
+        # set constants
         self._Vref         = vref
         self._AC_freq      = ac_freq
-        self._ADC_res      = 2.0**num_adc_bits
-        self._ADC_port     = adc_port
+        self._ADC_channel  = adc_channel
         self._Num_cal_avgs = num_cal_avgs
-        
+        self._ADC_scale    = 1.0 / self._ADC_res * self._Vref / self._sensitivity
+
+
+    def _read_adc_raw(self):
+        return self._mcp.read_adc(self._ADC_channel)
+
 
     # don't need to run calibration routine, if this is the case, zero will be set to 0 for unidirectional
     # and Fs/2 for bidirectional
     def _calibrate(self):
         # ensure that nothing is connected to the current sensor when running this
         # even better, tie input to ground
-        for i in range(self._Num_cal_avgs):
-            self._zero += self._mcp.read_adc(self._ADC_port)
+        for i in range(self._Num_cal_avgs - 1):
+            self._zero += self._mcp.read_adc(self._ADC_channel)
             time.sleep(0.25)
-        self._zero /= num_averages
+        self._zero /= self._Num_cal_avgs
         print('-I- current sensor calibrated successfully')
-        return self._zero
 
 
     def get_current_DC(self):
-        I = 1.0 * (self._zero - self._mcp.read_adc(adc_port)) / self._ADC_res * self._Vref / self._sensitivity
-        return I
+        I = 1.0 * (self._zero - self._read_adc_raw()) * self._ADC_scale 
+        return float(I)
 
 
     def get_voltage_DC(self):
-        V = 1.0 * self._sensitivity * get_current_DC()
-        return V
+        V = 1.0 * self._sensitivity * self.get_current_DC()
+        return float(V)
 
 
     def get_power_DC(self):
-        P = 1.0 * get_voltage_DC * get_current_DC()
-        return P
+        P = 1.0 * self.get_voltage_DC() * self.get_current_DC()
+        return float(P)
 
     # TBD
     # def get_energy_DC():
--- src/software/raspberry_pi/src/ACS712/examples/test_ACS712.py
+++ src/software/raspberry_pi/src/ACS712/examples/test_ACS712.py
@@ -2,17 +2,17 @@ import time
 import ACS712
 
 # ACS712 configuration
-ACS712_type     = 'ACS712_30A'
+ACS712_type     = 'ACS712_30AB'
 Vref            = 3.3
 num_adc_bits    = 10
 hardware_spi_en = True
-adc_port        = 0
+adc_channel     = 0
 num_cal_avgs    = 20
 
 current_sensor = ACS712.ACS712(device_type  = ACS712_type,
                                vref         = Vref,
                                num_adc_bits = num_adc_bits,
-                               adc_port     = adc_port,
+                               adc_channel  = adc_channel,
                                num_cal_avgs = num_cal_avgs,
                                hardware_spi = hardware_spi_en
                                )
@@ -23,12 +23,13 @@ print('-I- calibrating current sensor...')
 current_sensor._calibrate()
 
 print('Reading ACS712 value')
-print('| {0:>4} | {1:>4} | {2:>4} |'.format('current [I]', 'voltage [V]', 'power [W]'))
+print('| {:^11} | {:^11} | {:^11} | {:^11} |'.format('current [A]', 'voltage [V]', 'power [W]', 'Raw ADC'))
 
 while True:
     # read ADC channel 
     I = current_sensor.get_current_DC()
     V = current_sensor.get_voltage_DC()
     P = current_sensor.get_power_DC()
-    print('| {0:>4} | {1:>4} | {2:>4} |'.format(I, V, P))
+    adc_input = current_sensor._read_adc_raw()
+    print('| {:11.5f} | {:11.5f} | {:11.5f} | {:11d} |'.format(I, V, P, adc_input))
     time.sleep(0.5)
