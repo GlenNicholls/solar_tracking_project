ecdeb3266e5bc26ca797e749633299d994d160bc
Glen Nicholls <dkillers303@gmail.com>
2018-10-12 16:40:24 -0600

    starting to update tests

---
+++ src/DS3231/test/test_ds3231_configuration.py
@@ -0,0 +1,55 @@
+import sys
+import time
+import datetime
+import DS3231
+import system_monitor
+
+
+starttime = datetime.datetime.utcnow()
+
+# create RTC object
+i2c_port  = 1 # set to 0 if using gen 1 pi
+i2c_addr  = 0x68
+latitude  = 39.7392
+longitude = 104.9903
+rtc = DS3231.DS3231(i2c_port  = i2c_port,
+                    i2c_addr  = i2c_addr,
+                    latitude  = latitude,
+                    longitude = longitude)
+
+# create system monitor object
+sys_mon = system_monitor.system_monitor()
+
+print('-I- Monitoring DS3231 Information')
+
+# Configure RTC
+rtc.configure_rtc()
+
+def configure_rtc():
+    # Initial checks for time accuracy
+    print('-I- Checking time')
+    print('-I- RTCs current time: {}'.format(rtc.get_time_str()))
+    print('-I- Current NTP time: {}'.format(datetime.datetime.now()))
+
+    # update RTC if power was lost or if we have internet connection
+    print('-I- Checking to see if power was lost or if there is an internet connection')
+    if rtc.get_power_lost() and sys_mon.is_wlan_connected():
+        rtc.set_datetime_now()
+        print('-I- Power was lost, time updated to: {}'.format(rtc.get_time_str()))
+    elif rtc.get_power_lost() and not sys_mon.is_wlan_connected():
+        print('-E- Power was lost and no internet connection, cannot update time!')
+    elif not rtc.get_power_lost() and sys_mon.is_wlan_connected():
+        rtc.set_datetime_now()
+        print('-I- There is an internet connection and power was not lost')
+        print('-I- Time updated to: {}'.format(rtc.get_time_str()))
+    else:
+        print('-I- Power was not lost and no connection to update time')
+    return (rtc.get_datetime, datetime.datetime.now())
+
+
+def test_configure_rtc():
+    rtc_time, current_time = configure_rtc()
+    print(current_time-rtc_time)
+
+
+test_configure_rtc()
