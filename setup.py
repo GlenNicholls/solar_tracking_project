4a13d3b5bc883ddd9d4f5c169d365ea464e6d171
Glen Nicholls <dkillers303@gmail.com>
2018-10-12 19:22:01 -0600

    forgot comma. fuck me, right?

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     dependency_links  = [
                          # 'https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5',
                          # 'https://github.com/adafruit/Adafruit_Python_MCP3008/tarball/master#egg=Adafruit-MCP3008-1.0.2'],
-                         ]
+                         ],
     install_requires  = [
                          'Adafruit-GPIO>=0.6.5',
                          'Adafruit-MCP3008>=1.0.2',
