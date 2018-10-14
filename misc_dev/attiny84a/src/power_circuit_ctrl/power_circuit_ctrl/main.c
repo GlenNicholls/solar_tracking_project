2f659cc6aa6b5340cf1ab845d773c1d6b020cdca
Glen Nicholls <dkillers303@gmail.com>
2018-10-14 16:33:30 -0600

    re-formated code based on Tonys suggestions

--- misc_dev/attiny84a/src/power_circuit_ctrl/power_circuit_ctrl/main.c
+++ misc_dev/attiny84a/src/power_circuit_ctrl/power_circuit_ctrl/main.c
@@ -137,93 +137,100 @@
 // }
 
 
+static inline void initInterrupts(void)
+{
+  // Configure INT0 interrupt mode
+  MCUCR |= (0b01 << ISC00); // Mode: logic change
+
+  // General Interrupt Mask Register
+  //
+  // PCIE1 : 1 Enable pin change INT
+  // PCIE0 : 1 Enable pin change INT
+  // INT0  : 1 Enable external interrupt
+  GIMSK |= (1 << INT0) | (1 << PCIE0) | (1 << PCIE1);
+
+  // Pin Change Mask Registers
+  PCMSK0 |= (1 << PCINT7);
+  PCMSK1 |= (1 << PCINT9);
+}
+
+static inline void initMCU(void)
+{
+  // DDRA Port Directions
+  //
+  // DDA7: 1 input pi_tx_hold_on PCINT7
+  // DDA6: 0 DNC low-power
+  // DDA5: 0 DNC low-power
+  // DDA4: 0 DNC low-power
+  // DDA3: 0 output pi_rx_dev_mode
+  // DDA2: 0 DNC low-power
+  // DDA1: 0 FAULT output
+  // DDA0: 0 output en_power
+  DDRA |= (1 << PA7);
+  DDRA &= ~( (1 << PA6) | (1 << PA5) | (1 << PA4) | (1 << PA3) |
+             (1 << PA2) | (1 << PA1) | (1 << PA0) );
+
+  // Enable pullups on PA[7] input
+  // Enable pullups on PA[6:0] for low-power
+  PORTA |= (1 << PA7) | (1 << PA6) | (1 << PA5) | (1 << PA4) |
+           (1 << PA3) | (1 << PA2) | (1 << PA1) | (1 << PA0);
+
+  // DDRB Port Directions
+  //
+  // DDB3: 0 DNC low-power
+  // DDB2: 1 input rtc_ALRM_N
+  // DDB1: 1 input psh_button
+  // DDB0: 0 DNC low-power
+  DDRB |= (1 << PB2) | (1 << PB1);
+  DDRB &= ~( (1 << PB3) | (1 << PB0) );
+
+  // Enable pullups on PB[2:1] input
+  // Enable pullups on PB[3,0] for low-power
+  PORTB |= (1 << PB3) | (1 << PB2) | (1 << PB1) | (1 << PB0);
+
+  // Enable the MCUCR pullups
+  MCUCR &= ~(1 << PUD);
+
+  // todo: not sure how to configure clocks yet
+
+  // Power Reduction Register
+  //
+  // PRTIM1 : ??
+  // PRTIM0 : ??
+  // PRUSI  : ??
+  PRR |= (1 << PRADC); // disable ADC
+
+  // Analog Comparator Control/Status Register
+  ACSR |= (1 << ACD); // disable
+
+  // Sleep Mode
+  //
+  // BODS  : disable BOD during low-power?
+  // PUD   : ??
+  // BODSE : ??
+  // todo: would power-down be better since it halts clocks and only wakes on async events??
+  // todo: should SE only be enabled when desired and cleared upon wakeup??
+  MCUCR |= (1 << SE);     // Enable sleep
+  MCUCR |= (0b11 << SM0); // Mode: standby
+
+  // Configure INT
+  initInterrupts();
+
+  // Enable global interrupts by set I-bit in SREG to 1
+  sei();
+}
 // todo: Complete GIFR ISR Routines
 
 
 int main(void)
 {
-    // Configure DDRA Port Directions
-    //
-    // DDA7: 1 input pi_tx_hold_on PCINT7
-    // DDA6: 0 DNC low-pwr
-    // DDA5: 0 DNC low-pwr
-    // DDA4: 0 DNC low-pwr
-    // DDA3: 0 output pi_rx_dev_mode
-    // DDA2: 0 DNC low-pwr
-    // DDA1: 0 FAULT output
-    // DDA0: 0 output en_power
-    DDRA |= (1 << PA7);
-    DDRA &= ~( (1 << PA6) | (1 << PA5) | (1 << PA4) | (1 << PA3) |
-               (1 << PA2) | (1 << PA1) | (1 << PA0) );
+  initMCU();
 
 
-    // Configure DDRB Port Directions
-    //
-    // DDB3: 0 DNC low-pwr
-    // DDB2: 1 input rtc_ALRM_N
-    // DDB1: 1 input psh_button
-    // DDB0: 0 DNC low-pwr
-    DDRB |= (1 << PB2) | (1 << PB1);
-
-
-    // PB1 (PCINT9 on PCIE1) : active low input from DS3231
-    // PB2 (INT0)            : active low input from push button (debounced in analog)
-
-
-
-    // Configure the General Interrupt Mask Register
-    //
-    // PCIE1 : 1 Enable pin change INT
-    // PCIE0 : 1 Enable pin change INT
-    // INT0  : 1 Enable external interrupt
-    // and set I-bit in SREG to one
-    GIMSK |= (1 << INT0) | (1 << PCIE0) | (1 << PCIE1);
-    sei();
-
-
-    // Configure the Pin Change Mask Registers
-    //
-    // Enable specific pin change interrupts in the
-    // pin change mask reg
-    PCMSK0 |= (1 << PCINT7);
-    PCMSK1 |= (1 << PCINT9);
-
-
-    // Configure MCU Control Register
-    //
-    // BODS  : disable BOD during low-power?
-    // PUD   : ??
-    // SE    : 1 Enable sleep enable
-    // SM1   : 1
-    // SM0   : 1 Enable standby sleep mode
-    // ISC00 : 1
-    // ISC01 : 0 Any INT0 level change generates INT
-    // BODSE : ??
-    // todo: would power-down be better since it halts clocks and only wakes on async events??
-    // todo: should SE only be enabled when desired and cleared upon wakeup??
-    MCUCR |=  (1 << SE) | (1 << SM1) | (1 << SM0) | (1 << ISC00);
-    MCUCR &= ~(1 << ISC01);
-
-
-    // Configure the Power Reduction Register
-    //
-    // PRTIM1 : ??
-    // PRTIM0 : ??
-    // PRUSI  : ??
-    // PRADC  : 1 to disable ADC
-    PRR |= (1 << PRADC);
-
-
-    // Configure Analog Comparator Control/Status Register
-    //
-    // ACD: 1 switch off analog comparator
-    ACSR |= (1 << ACD);
-
-
-    while (1)
-    {
-        sleep();
-        //_delay_ms(200);
-    }
+  while (1)
+  {
+      sleep();
+      //_delay_ms(200);
+  }
 }
 
