d705f8e5ff945edb359cbdd0cb7a17133849ab2d
Glen Nicholls <dkillers303@gmail.com>
2018-10-15 18:57:49 -0600

    updated power ctrl stuff to ensure it works

--- misc_dev/attiny84a/src/power_circuit_ctrl/power_circuit_ctrl/main.c
+++ misc_dev/attiny84a/src/power_circuit_ctrl/power_circuit_ctrl/main.c
@@ -169,7 +169,7 @@ static inline void initMCU(void)
   // DDA0: 0 output en_power
   DDRA &= ~(1 << PA7);
   DDRA |= (1 << PA6) | (1 << PA5) | (1 << PA4) | (1 << PA3) |
-          (1 << PA2) | (1 << PA1) | (1 << PA0);
+    (1 << PA2) | (1 << PA1) | (1 << PA0);
 
   // Enable pullups on PA[7] input
   // Enable pullups on PA[6:0] for low-power
@@ -178,41 +178,43 @@ static inline void initMCU(void)
 
   // DDRB Port Directions
   //
-  // DDB3: 0 DNC low-power
-  // DDB2: 1 input rtc_ALRM_N
-  // DDB1: 1 input psh_button
-  // DDB0: 0 DNC low-power
-  DDRB |= (1 << PB2) | (1 << PB1);
-  DDRB &= ~( (1 << PB3) | (1 << PB0) );
+  // DDB3: 1 DNC low-power
+  // DDB2: 0 input rtc_ALRM_N
+  // DDB1: 0 input psh_button
+  // DDB0: 1 DNC low-power
+  DDRB &= ~( (1 << PB2) | (1 << PB1) );
+  //DDRB |= (1 << PB3) | (1 << PB0);
 
   // Enable pullups on PB[2:1] input
   // Enable pullups on PB[3,0] for low-power
-  PORTB |= (1 << PB3) | (1 << PB2) | (1 << PB1) | (1 << PB0);
+  //PORTB |= (1 << PB3) | (1 << PB2) | (1 << PB1) | (1 << PB0);
+  PORTB |= (1 << PB3) | (1 << PB1) | (1 << PB0);
+  PORTB &= ~(1 << PB2); // debugging pullup
 
   // Enable the MCUCR pullups
   MCUCR &= ~(1 << PUD);
 
   // todo: not sure how to configure clocks yet
-
-  // Analog Comparator Control/Status Register
-  ACSR |= (1 << ACD); // disable
-
-  // Power Reduction Register
-  //
-  // PRTIM1 : ??
-  // PRTIM0 : ??
-  // PRUSI  : ??
-  PRR |= (1 << PRADC); // disable ADC
-
-  // Sleep Mode
-  //
-  // BODS  : disable BOD during low-power?
-  // PUD   : ??
-  // BODSE : ??
-  // todo: would power-down be better since it halts clocks and only wakes on async events??
-  // todo: should SE only be enabled when desired and cleared upon wakeup??
-  MCUCR |= (1 << SE);     // Enable sleep
-  MCUCR |= (0b11 << SM0); // Mode: standby
+//
+//  // Analog Comparator Control/Status Register
+//  ACSR |= (1 << ACD); // disable
+//
+//  // Power Reduction Register
+//  //
+//  // PRTIM1 : ??
+//  // PRTIM0 : ??
+//  // PRUSI  : ??
+//  PRR |= (1 << PRADC); // disable ADC
+//
+//  // Sleep Mode
+//  //
+//  // BODS  : disable BOD during low-power?
+//  // PUD   : ??
+//  // BODSE : ??
+//  // todo: would power-down be better since it halts clocks and only wakes on async events??
+//  // todo: should SE only be enabled when desired and cleared upon wakeup??
+//  MCUCR |= (1 << SE);     // Enable sleep
+//  MCUCR |= (0b11 << SM0); // Mode: standby
 
   // Configure INT
   initInterrupts();
@@ -223,19 +225,10 @@ static inline void initMCU(void)
 
 // todo: Complete GIFR clear here
 // todo: wait on this until blink is accomplished first
-// ISR(PCINT1_vect)
-// {
-//
-// }
-//
-// ISR(PCINT0_vect)
-// {
-//
-// }
-
+// todo: will an interrupt while another interrupt is being serviced cause contention on en_power??
 ISR(EXT_INT0_vect)
 {
-  if ( (PINB & (1 << PINB2)) == 0 )
+  if (PINB & (1 << PB2)) // pb2 high
   {
     PORTA |= (1 << PA0); // turn LED on
   }
@@ -245,6 +238,29 @@ ISR(EXT_INT0_vect)
   }
 }
 
+ISR(PCINT0_vect)
+{
+
+
+  // Clear interrupt flag
+  GIFR |= (1 << PCIF1);
+}
+
+ISR(PCINT1_vect)
+{
+  if (PINB & (1 << PB1)) // pb2 high
+  {
+    PORTA |= (1 << PA0); // turn LED on
+  }
+  else
+  {
+    PORTA &= ~(1 << PA0); // turn LED off
+  }
+
+  // Clear interrupt flag
+  GIFR |= (1 << INTF0);
+}
+
 
 int main(void)
 {
@@ -252,8 +268,7 @@ int main(void)
 
   while (1)
   {
-    sleep_cpu();
-    //_delay_ms(200);
+    //sleep_cpu();
   }
 
   return 0;
