1bcb199607d44e8dadd840b503b58a9c528dc2ee
Glen Nicholls <dkillers303@gmail.com>
2018-10-18 16:44:44 -0600

    re-organized hierarchy and filename

--- misc_dev/attiny84a/src/pi_hat_ctrl/pi_hat_ctrl.c
+++ misc_dev/attiny84a/src/pi_hat_ctrl/pi_hat_ctrl.c
@@ -124,72 +124,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
-//#include <util/delay.h>
-
-// todo: what does the F_CPU do hardware wise? Does changing this value use hardware freq. synth. or
-//       is it simply a software definition for something that is embedded??
-#define F_CPU 8000000 // todo: lower clock... dummy
-//#define F_CPU 1000000UL
-
-// used for a very short delay
-#define _NOP() do { __asm__ __volatile__ ("nop"); } while (0)
-
-// define pins for readability
-// todo: put this junk into .h file
-#define POWER_ON_PIN   PA0
-#define FAULT_PIN      PA1
-#define DEV_MODE_PIN   PA3
-#define DEVICE_ACK_PIN PA7
-#define BUTTON_PIN     PB1
-#define RTC_ALARM_PIN  PB2
-
-#define POWER_ON_PORT   PORTA
-#define FAULT_PORT      PORTA
-#define DEV_MODE_PORT   PORTA
-#define DEVICE_ACK_PORT PORTA
-#define BUTTON_PORT     PORTB
-#define RTC_ALARM_PORT  PORTB
-
-#define TURN_POWER_ON     SET_BIT(POWER_ON_PORT, POWER_ON_PIN)
-#define TURN_POWER_OFF    CLR_BIT(POWER_ON_PORT, POWER_ON_PIN)
-#define TURN_FAULT_ON     SET_BIT(FAULT_PORT, FAULT_PIN)
-#define TURN_FAULT_OFF    CLR_BIT(FAULT_PORT, FAULT_PIN)
-#define TURN_DEV_MODE_ON  SET_BIT(DEV_MODE_PORT, DEV_MODE_PIN)
-#define TURN_DEV_MODE_OFF CLR_BIT(DEV_MODE_PORT, DEV_MODE_PIN)
-
-// functions for checking pin states
-// todo: should getSomething convention be used instead??
-static inline bool isPowerOn(void)
-{
-  return (POWER_ON_PORT & (1 << POWER_ON_PIN)) > 0;
-}
-
-static inline bool isFaultOn(void)
-{
-  return (FAULT_PORT & (1 << FAULT_PIN)) > 0;
-}
-
-static inline bool isDevModeOn(void)
-{
-  return (DEV_MODE_PORT & (1 << DEV_MODE_PIN)) > 0;
-}
-
-static inline bool isDeviceAckOn(void)
-{
-  return (DEVICE_ACK_PORT & (1 << DEVICE_ACK_PIN)) > 0;
-}
-
-static inline bool isButtonOn(void) // look for low-going edge (active low)
-{
-  return (BUTTON_PORT & (1 << BUTTON_PIN)) == 0;
-}
-
-static inline bool isRTCAlarmOn(void) // look for low-going edge (active low)
-{
-  return (RTC_ALARM_PORT & (1 << RTC_ALARM_PIN)) == 0;
-}
+#include <pi_hat_ctrl.h>
 
 
+// todo: should the init functions be put into a .h as well??
 // todo: implement this when the time comes
 // void WDT_off(void)
 // {
@@ -326,27 +264,42 @@ static inline void initMCU(void)
 /*
  * RTC Alarm ISR
  */
+// ISR(EXT_INT0_vect)
+// {
+//   if isAlarmOn() // Alarm has occured
+//   {
+//     if isPowerOn() // Checking to see if load switch already on
+//     {
+//       // todo: Should FAULTS ever be cleared before user intervention??
+//       TURN_FAULT_ON; // raise FAULT as this shouldn't happen, but maintain power to device
+//     }
+//     else
+//     {
+//       TURN_POWER_ON; // Turn load switch on
+//     }
+//   }
+//   // todo: find out if any error checking needs to be done when alarm is cleared
+//   //       should be branching back to service other interrupts or sleep
+//   // else {}
+//
+//   // Insert nop for synchronization
+//   _NOP();
+// }
+
 ISR(EXT_INT0_vect)
 {
   if isAlarmOn() // Alarm has occured
   {
-    if isPowerOn() // Checking to see if load switch already on
-    {
-      // todo: Should FAULTS ever be cleared before user intervention??
-      TURN_FAULT_ON; // raise FAULT as this shouldn't happen, but maintain power to device
-    }
-    else
-    {
-      TURN_POWER_ON; // Turn load switch on
-    }
+    TURN_POWER_ON; // Turn load switch on
   }
-  // todo: find out if any error checking needs to be done when alarm is cleared
-  //       should be branching back to service other interrupts or sleep
-  // else {}
-
+  else
+  {
+    TURN_POWER_OFF;
+  }
   // Insert nop for synchronization
   _NOP();
 }
+
 
 /*
  * pi_tx_hold_on ISR
---
+++ misc_dev/attiny84a/src/pi_hat_ctrl/pi_hat_ctrl.h
@@ -0,0 +1,68 @@
+// todo: what does the F_CPU do hardware wise? Does changing this value use hardware freq. synth. or
+//       is it simply a software definition for something that is embedded??
+#define F_CPU 8000000 // todo: lower clock... dummy
+//#define F_CPU 1000000UL
+
+// used for a very short delay
+#define _NOP() do { __asm__ __volatile__ ("nop"); } while (0)
+
+// define pins for readability
+// todo: put this junk into .h file
+#define POWER_ON_PIN   PA0
+#define FAULT_PIN      PA1
+#define DEV_MODE_PIN   PA3
+#define DEVICE_ACK_PIN PA7
+#define BUTTON_PIN     PB1
+#define RTC_ALARM_PIN  PB2
+
+#define POWER_ON_PORT   PORTA
+#define FAULT_PORT      PORTA
+#define DEV_MODE_PORT   PORTA
+#define DEVICE_ACK_PORT PORTA
+#define BUTTON_PORT     PORTB
+#define RTC_ALARM_PORT  PORTB
+
+#define TURN_POWER_ON     SET_BIT(POWER_ON_PORT, POWER_ON_PIN)
+#define TURN_POWER_OFF    CLR_BIT(POWER_ON_PORT, POWER_ON_PIN)
+#define TURN_FAULT_ON     SET_BIT(FAULT_PORT, FAULT_PIN)
+#define TURN_FAULT_OFF    CLR_BIT(FAULT_PORT, FAULT_PIN)
+#define TURN_DEV_MODE_ON  SET_BIT(DEV_MODE_PORT, DEV_MODE_PIN)
+#define TURN_DEV_MODE_OFF CLR_BIT(DEV_MODE_PORT, DEV_MODE_PIN)
+
+// Timer defines
+// F_CPU/(prescaler*(timer_max_val - timer_preset)) = num_timer_OVF_per_sec
+// todo: setFlag() for checking timer in other ISR
+
+
+
+// functions for checking pin states
+// todo: should getSomething convention be used instead??
+static inline bool isPowerOn(void)
+{
+  return (POWER_ON_PORT & (1 << POWER_ON_PIN)) > 0;
+}
+
+static inline bool isFaultOn(void)
+{
+  return (FAULT_PORT & (1 << FAULT_PIN)) > 0;
+}
+
+static inline bool isDevModeOn(void)
+{
+  return (DEV_MODE_PORT & (1 << DEV_MODE_PIN)) > 0;
+}
+
+static inline bool isDeviceAckOn(void)
+{
+  return (DEVICE_ACK_PORT & (1 << DEVICE_ACK_PIN)) > 0;
+}
+
+static inline bool isButtonOn(void) // look for low-going edge (active low)
+{
+  return (BUTTON_PORT & (1 << BUTTON_PIN)) == 0;
+}
+
+static inline bool isRTCAlarmOn(void) // look for low-going edge (active low)
+{
+  return (RTC_ALARM_PORT & (1 << RTC_ALARM_PIN)) == 0;
+}
