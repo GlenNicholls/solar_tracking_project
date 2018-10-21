147dc4b6e08c39a0c44649bcff2c43947e6e8a60
bradhuber1 <bradhuber1@yahoo.com>
2018-10-20 23:35:48 -0600

    Made some more changes to main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
 from datetime import datetime
+from datetime import timedelta
 import time
 import logging
 from astral import Astral
+from astral import Location
 import geocoder
+import pytz
+import os
 
 #NOTE: indentation is 2 spaces
   
@@ -11,68 +16,46 @@ import geocoder
 # TODO: Pass logger class or however you do it to sub-packages
 
 #Global area
-#wait time for easy readout of algorithm, will remove once everythin is implemented
-t = 2
-
 main_logger = logging.getLogger('main_logger')
 main_logger.setLevel(logging.DEBUG)
 # create console handler to log to the terminal
 ch = logging.StreamHandler()
+# create file handler to log the state for next reboot
+#fh = logging.FileHandler('state.log')
 # set logging level to debug, will switch to info for final version
 ch.setLevel(logging.DEBUG)
+#fh.setLevel(logging.DEBUG)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 # add the handlers to logger
 main_logger.addHandler(ch)
-
-lat = ""
-long = ""
-elev = ""
 #End Global area
 
-# todo: do not put all verbose config in single function
-#       split out into setupPackageName(params) and define params
-#       and call function here - GN
-def setup():
-  
-  #Laod stored parameters
-  main_logger.info('Loading stored prarmeters')
-  #need to load from parameter file or similar, hard coded for now
-  lat = 37.8
-  long = -97.8
-  elev = 6000
-  time.sleep(t)
-  
-  #Load user specifice parameters
-  main_logger.warn('Loading user specified parameters NOT DEFINED')
-  time.sleep(t)
-  
-  #Get Location
-  getLocation()
-  
-  #Calibrate system
-  main_logger.info('Calibrating System NOT DEFINED')
-  time.sleep(t)
-  
-#End setup()
-
-# todo: possibly abstract some astral wrappers elsewhere -GN
-def getLocation():
-  global lat, long, elev
+def get_location(lat, lng):
   try:
     main_logger.info('Getting location from IP')
     g = geocoder.ip('me')
     lat = g.latlng[0]
-    long = g.latlng[1]
+    lng = g.latlng[1]
     main_logger.info('Get location from IP Successfull! =)')
   except:
     main_logger.warn('Get location from IP Failed, using defaults. =(')
 
-  main_logger.info("Lattitude: [" + str(lat) + "], Longitude: [" + str(long) + "]")
-  main_logger.info("Elevation: " + str(elev))
-  
-#End getLocation()
+  main_logger.info("Lattitude: [" + str(lat) + "], Longitude: [" + str(lng) + "]")
+  return lat, lng 
+#End get_location
+
+def get_location_astral(lat, lng, elev):
+  loc = Location()
+  loc.name = 'solar_tracker'
+  loc.region = 'whereIam'
+  loc.latitude = lat
+  loc.longitude = lng
+  loc.timezone = 'US/Mountain'
+  loc.elevation = elev
+  return loc
+#End get_location_astral
 
 '''
 todo: list for uC stuff -GN
@@ -85,51 +68,83 @@ todo: list for uC stuff -GN
    >>> shutdown -h now immediately
 5) will think about other cases that need to be accounted for in here
 '''
+
+def shutdown():
+  main_logger.info('Initiating Shutdown')
+  #os.system('shutdown now -h')
+#End shutdown
+
 def main():
   # todo: very first step should be using the check_and_clear_alarms() from DS3231
   #       and verifying again that the alarms were cleared. If not, user needs to 
   #       be notified and some checks about if we can talk to the device should be done.
   #       if this happens, system cannot shutdown
+ 
+  main_logger.info("Current UTC time: " + str(datetime.utcnow()))
   
-  utc_now = datetime.utcnow()
-  mm = str(utc_now.month)
-  dd = str(utc_now.day)
-  yyyy = str(utc_now.year)
-  hour = str(utc_now.hour)
-  mi = str(utc_now.minute)
-  ss = str(utc_now.second)
-  
-  #US/Mountain  = timezone
-  # todo: not sure we want to use utc time per mike's suggestions a couple weeks back
-  #       ds3231 package converts to local, so logging time might be confusing and miss-aligned -GN
-  # todo: For this, since you have datetime object, can use .strptime() or whatever the other one
-  #       is to return utc time as specified parsed-order string -GN
-  main_logger.info("Current UTC time: [" + mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss + "]")
-
-  #Run setup
+  #Run setup if needed
   main_logger.info('Running setup')
-  setup()
-  time.sleep(t)
+  
+  #Load stored parameters
+  main_logger.info('Loading stored prarmeters')
+  #need to load from parameter file or similar, hard coded for now
+  lat = 37.8
+  long = -97.8
+  elev = 6000
+  
+  #Load user specifice parameters
+  main_logger.warn('Loading user specified parameters NOT DEFINED')
+  
+  #Get Location
+  lat, lng = get_location(lat, long)
+  
+  #Get astral with current location
+  loc_astral = get_location_astral(lat, lng, elev)
+  
+  #Calibrate system
+  main_logger.info('Calibrating System NOT DEFINED')
+  
+  sun_dict = loc_astral.sun()
+  now = pytz.timezone('US/Mountain').localize(datetime.now())
   
   #Get current position from shart encoders
   main_logger.warn('Get current position from shaft encoders NOT DEFINED')
-  time.sleep(t)
   
   #Get current position from motors
   main_logger.warn('Get current position from motors NOT DEFINED')
-  time.sleep(t)
   
-  #Calculate sun position
-  main_logger.debug('Calculate sun position')
-  a = Astral()
-  solarAz = a.solar_azimuth(datetime.utcnow(), lat, long)
-  solarEl = a.solar_elevation(datetime.utcnow(), lat, long)
-  main_logger.info("Current Solar Azimuth: [" + str(solarAz) + "], Current Solar Elevation: [" + str(solarEl) + "]")
-  time.sleep(t) # todo: instead of sleeping, should be powering down
+  if now > sun_dict['sunrise'] and now < sun_dict['sunset']:
+    daytime = True
+    print("Daytime")
+  else:
+    daytime = False
+    print("Nighttime")
+    
+  if not daytime: #this will be the if check from above, implemented this way for development
+    #Get solar position
+    solar_az = loc_astral.solar_azimuth(datetime.now())
+    solar_el = loc_astral.solar_elevation(datetime.now())
+    main_logger.info("Next Solar Azimuth: [" + str(solar_az) + "], Next Solar Elevation: [" + str(solar_el) + "]")
+    
+    #Move to calculated sun posistion
+    main_logger.info('Moving to next position NOT DEFINED')
+    
+    #Read light sensor
+    main_logger.info('Reading light sensor')
+    
+  else:
+    #Get solar position for tomorrow morning
+    tomorrow = datetime.today() + timedelta(days=1)
+    sun_dict = loc_astral.sun(tomorrow.date())
+    solar_az = loc_astral.solar_azimuth(sun_dict['sunrise'])
+    solar_el = loc_astral.solar_elevation(sun_dict['sunrise'])
+    main_logger.info("Tomorrow Solar Azimuth: [" + str(solar_az) + "], Tomorrow Solar Elevation: [" + str(solar_el) + "]")
+    
+    #Move to sunrise position for tomorrow
+    main_logger.info('Moving to sunrise position for tomorrow NOT DEFINED')
+  #End if else
 
-  # todo: call something here like initiateShutdownRoutine()
-  #       I can take care of writing this when I get a chance as
-  #       we have uC processes to take care of via GPIO -GN
+  shutdown()
   
 #End main()
 
