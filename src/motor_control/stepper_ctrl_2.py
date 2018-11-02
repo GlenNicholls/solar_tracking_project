9757cbeb1d44b309fd8fdecc6c9e53d2b493baaa
bradhuber1 <bradhuber1@yahoo.com>
2018-11-01 19:16:55 -0600

    Created stepper_motor class and updated comments in stepper_ctrl_2

--- src/motor_control/stepper_ctrl_2.py
+++ src/motor_control/stepper_ctrl_2.py
@@ -11,25 +11,25 @@ for pin in lines:  #sets pins 12,13,16,19,26 as outputs
 ctl = 0
 cnt = 0
 dir = 1
-MOT.output(16,1)  #sets clock pin high
+MOT.output(16,1)  #sets clock pin high, falling edge
 MOT.output(12,dir)  #sets motor direction 1 = W/N, 0 = E/S
 MOT.output(13,0)  #disable EL controller
 MOT.output(19,0)  #disable AZ contyroller
 MOT.output(26,1)  #resets to starting configuration 1010
-MOT.output(26,0)
-MOT.output(26,1)
+MOT.output(26,0)  #starts reset
+MOT.output(26,1)  #ends reset
 MOT.output(19,1)  #enable AZ  controller
 MOT.output(13,1) #enable EL controller
-td = .005
-steps = 72  #sets number of steps
+td = .005  #controls the number of steps
+steps = 72  #sets number of steps (72 = 1 degree elevation) (22 = 1 degree azimuth)
 for clk in range(0,steps):  #run number of steps
     cnt = cnt+1
     if cnt == 22:
-        MOT.output(19,0)
+        MOT.output(19,0) #disables az controller
     MOT.output(16,0)  #pulse the clock pin                         
     time.sleep(td)
     MOT.output(16,1)
     time.sleep(td)
-MOT.output(19,0)  #disable controller
-MOT.output(13,0)
+MOT.output(19,0)  #disable az controller
+MOT.output(13,0)  #disable el controller
 cnt = 0
---
+++ src/motor_control/stepper_motor.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as MOT
+import time
+
+#TODO: Define a constants file???
+#Constants
+WEST = 1
+NORTH = 1
+EAST = 0
+SOUTH = 0
+ENABLE = 1
+DISABLE = 0
+DIR = 12 #12-direction (17
+EL = 13 #13-EL controller enable (10)
+CLK = 16 #16-clock (18)
+AZ = 19 #19-AZ controller enable (10)
+RST = 26 #26-reset (20
+DEGAZ = 22 #Number of steps for 1 degree of movement in azimuth
+DEGEL = 72 #Number of steps for 1 degree of movement in elevation
+SPEED = 0.005 #Time delay to control the speed
+
+class stepper_motor:
+
+  def __init__(self) 
+    MOT.setwarnings(False)
+    MOT.setmode(MOT.BCM)
+    lines = [DIR, EL, CLK, AZ, RST]
+    for pin in lines:  #sets pins 12,13,16,19,26 as outputs
+        MOT.setup(pin,MOT.OUT)
+  #End __init__
+  
+  #TODO: Pass parameters using enum
+  def move_motor(axis, dir, deg)
+    if axis = AZ:
+      steps = deg*DEGAZ
+    elif axis = EL:
+      steps = deg*DEGEL
+    else:
+      return
+      
+    MOT.output(CLK,ENABLE)  #sets clock pin high, falling edge
+    MOT.output(DIR,dir)  #sets motor direction 1 = W/N, 0 = E/S
+    MOT.output(axis,DISABLE)
+    MOT.output(RST,1)  #resets to starting configuration 1010
+    MOT.output(RST,0)  #starts reset
+    MOT.output(RST,1)  #ends reset
+    MOT.output(axis,ENABLE)
+      
+    for step in range(0, steps)
+      MOT.output(CLK, DISABLE) #pulse the clock pin
+      time.sleep(SPEED)
+      MOT.output(CLK, ENABLE)
+      time.sleep(SPEED)
+    
+    MOT.output(axis, DISABLE)
+  #End move_motor
+  
+#End stepper_motor
