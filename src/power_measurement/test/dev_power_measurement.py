d8c0800d9270928345e391928beb7918c74042e1
Glen Nicholls <dkillers303@gmail.com>
2018-11-06 13:59:25 -0700

    updated dev file and renamed test

--- src/power_measurement/test/dev_power_measurement.py
+++ src/power_measurement/test/dev_power_measurement.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 
 from test_mcp3008 import *
+from test_current_measurement import *
 import time
 
-
+# adc test
 test_adc_read()
+
+# power test
+test_panel_current()
+test_panel_voltage()
+test_panel_power()
+
+test_battery_current()
+test_battery_voltage()
+test_battery_power()
 
