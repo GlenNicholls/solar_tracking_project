abf6216847b46e178da2fa546de0279f9cbe5690
Glen Nicholls <dkillers303@gmail.com>
2018-11-07 18:58:05 -0700

    finalized everything in sun sensor logic package for main.... I dont test because im BA

--- src/sun_sensor/sun_sensor.py
+++ src/sun_sensor/sun_sensor.py
@@ -7,18 +7,24 @@ class sun_sensor(object):
 
     def __init__ (self, main_logger       = 'main_logger',
                        logger_module_name = 'sun_sensor',
-                       adc_volt_ref       = 3.3,
-                       adc_num_bits       = 10,
-                       adc_nw_sens_ch     = None,# north west
-                       adc_ne_sens_ch     = None,# north east
-                       adc_sw_sens_ch     = None,# south west
-                       adc_se_sens_ch     = None,# south east
-                       adc_object         = None
+                       move_motor_thresh_perc = None
+                       adc_volt_ref   = 3.3,
+                       adc_num_bits   = 10,
+                       adc_ur_sens_ch = None, # upper right sensor channel
+                       adc_ul_sens_ch = None, # upper left sensor channel
+                       adc_lr_sens_ch = None, # lower right sensor channel
+                       adc_ll_sens_ch = None, # lower left sensor channel
+                       adc_object     = None
                   ):
 
         # instantiate logger
         self.logger = logging.getLogger(main_logger + '.' + logger_module_name)
         self.logger.info('creating an instance of the {}'.format(logger_module_name))
+
+        # logic thresh
+        self._thresh_perc = move_motor_thresh_perc
+        if self._thresh_perc == None or self._thresh_perc <= 0.0 or self._thresh_perc >= 100.0:
+            raise ValueError('Invalid threshold for motor movement, must be float [0-100]!')
 
         # capture internal config
         self._v_ref        = adc_volt_ref
@@ -28,17 +34,22 @@ class sun_sensor(object):
         self._adc_ch  = adc_channel
         self._adc     = adc_object
 
-        adc_ch_tuple = (adc_nw_sens_ch, adc_ne_sens_ch, adc_sw_sens_ch, adc_se_sens_ch)
+        adc_ch_tuple = (adc_ur_sens_ch, adc_ul_sens_ch, adc_lr_sens_ch, adc_ll_sens_ch)
         if len(adc_ch_tuple) > len(set(adc_ch_tuple)): # appending tuples to check for uniqueness
             raise ValueError('ADC channels are not unique!')
 
-        if adc_nw_sens_ch not in range(8):
+        self._ur = adc_ur_sens_ch
+        self._ul = adc_ul_sens_ch
+        self._lr = adc_lr_sens_ch
+        self._ll = adc_ll_sens_ch
+
+        if self._ur not in range(8):
             raise ValueError('Invalid ADC channel, must be int 0-7!')
-        if adc_ne_sens_ch not in range(8):
+        if self._ul not in range(8):
             raise ValueError('Invalid ADC channel, must be int 0-7!')
-        if adc_sw_sens_ch not in range(8):
+        if self._lr not in range(8):
             raise ValueError('Invalid ADC channel, must be int 0-7!')
-        if adc_se_sens_ch not in range(8):
+        if self._ll not in range(8):
             raise ValueError('Invalid ADC channel, must be int 0-7!')
 
         if adc_object == None: # not sure how to check for class
@@ -72,7 +83,7 @@ class sun_sensor(object):
 
 
     def eval_horizontal(self, avg_diff_horizon):
-        if abs(avg_diff_horizon) > per_diff_threshold:
+        if abs(avg_diff_horizon) > self._thresh_perc:
             if avg_diff_horizon < 0.0:
                 self.logger.debug('Moving horizon left')
                 return 1 # move left
@@ -84,14 +95,76 @@ class sun_sensor(object):
             return 0
 
 
+    # TODO: use single eval function for both vertical/horizontal
     def eval_vertical(self, avg_diff_vertical):
-        if abs(avg_diff_vertical) > per_diff_threshold:
+        if abs(avg_diff_vertical) > self._thresh_perc:
             if avg_diff_vertical < 0.0:
                 self.logger.debug('Moving vertical up')
                 return -1 # move left
             else:
                 self.logger.debug('Moving vertical down')
-                return -1 # move right
+                return 1 # move right
         else:
             self.logger.debug('Not greater than thresh, not updating vertical')
             return 0
+
+
+    def get_diff_upper_perc(self):
+        diff = self.__get_per_diff(self._ul, self._ur)
+        self.logger.debug('Sun sensor percent difference for upper left and upper right: {}'.format(diff))
+        return diff
+
+
+    def get_diff_lower_perc(self):
+        diff = self.__get_per_diff(self._ll, self._lr)
+        self.logger.debug('Sun sensor percent difference for lower left and lower right: {}'.format(diff))
+        return diff
+
+
+    def get_diff_left_perc(self):
+        diff = self.__get_per_diff(self._ul, self._ll)
+        self.logger.debug('Sun sensor percent difference for upper left and lower left: {}'.format(diff))
+        return diff
+
+
+    def get_diff_right_perc(self):
+        diff = self.__get_per_diff(self._ur, self._lr)
+        self.logger.debug('Sun sensor percent difference for upper right and lower right: {}'.format(diff))
+        return diff
+
+
+    def get_all_diff_perc(self):
+        upper = self.get_diff_upper_perc()
+        lower = self.get_diff_lower_perc()
+        left  = self.get_diff_left_perc()
+        right = self.get_diff_right_perc()
+        return (upper, lower, left, right)
+
+
+    # TODO: change all horizon/vertical to azimuth/elevation to make clear
+    def get_avg_horizon(self):
+        upper, lower, _, _ = self.get_all_diff_perc()
+        avg = self.__get_avg(upper, lower)
+        self.logger.debug('Sun sensor horizon average: {}'.format(avg))
+        return avg
+
+    def get_avg_vertical(self):
+        _, _, left, right = self.get_all_diff_perc()
+        avg = self.__get_avg(left, right)
+        self.logger.debug('Sun sensor vertical average: {}'.format(avg))
+        return avg
+
+
+    def get_all_avg(self):
+        horizon  = self.get_avg_horizon()
+        vertical = self.get_avg_vertical()
+        return (horizon, vertical)
+
+
+    # TODO: make better name
+    def is_move_motor(self):
+        horizon, vertical = self.get_all_avg()
+        move_horiz_mot = self.eval_horizontal(horizon)
+        move_vert_mot = self.eval_horizontal(vertical)
+        return (move_horiz_mot, move_vert_mot)
+
