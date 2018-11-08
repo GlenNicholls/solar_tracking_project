1c55b7cce4734f6c4d72b860d3bcd8fcc2dc01db
Glen Nicholls <dkillers303@gmail.com>
2018-11-08 16:41:54 -0700

    added dev script for development

---
+++ src/sun_sensor/test/dev_sun_sensor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from sun_sensor import *
+
+test_sens_channels()
+test_sun_sensor_differences()
+test_sun_sensor_averages()
+test_motor_move_flags()
+test_motor_move_direction()
+
