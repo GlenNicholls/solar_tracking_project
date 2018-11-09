98a2c8d72c345a19b852d97b2ac5f07fb5a08f06
Glen Nicholls <dkillers303@gmail.com>
2018-11-08 22:10:40 -0700

    added logging capability for shaft encoder package

--- src/shaft_encoder/encoder.py
+++ src/shaft_encoder/encoder.py
@@ -3,13 +3,38 @@ import RPi.GPIO as GPIO
 
 class encoder:
 
-  def __init__(self,a_pin,b_pin, ppr):
+  def __init__(self, logger_name        = 'main_logger',
+                     logger_module_name = 'shaft_encoder',
+                     a_pin              = None,
+                     b_pin              = None, 
+                     ppr                = 0
+                ):
+    # instantiate logger
+    self.logger = logging.getLogger(logger_name + '.' + logger_module_name)
+    self.logger.info('creating an instance of the {}'.format(logger_module_name))
+
     # Define pin numbers for A/B channels on azimuth/elevation motors
     self.A_pin=a_pin
     self.B_pin=b_pin
 
+    if len((self.A_pin, self.B_pin)) > len(set((self.A_pin, self.B_pin))):
+      raise ValueError('GPIO pin numbers must be unique integers!')
+    if self.A_pin == None or type(self.A_pin) != int:
+      raise ValueError('GPIO Must be integer!')
+    else:
+      self.logger.debug('Shaft encoder A pin: {}'.format(A_pin))
+    if self.B_pin == None or type(self.B_pin) != int:
+      raise ValueError('GPIO Must be integer!')
+    else:
+      self.logger.debug('Shaft encoder B pin: {}'.format(B_pin))
+
     # Define pulses per revolution (2000 for chosen encoders)
     self.ppr = ppr
+
+    if type(self.ppr) != int or self.ppr == 0:
+      raise ValueError('GPIO Must be integer >0!')
+    else:
+      self.logger.debug('Shaft encoder PPR: {}'.format(ppr))
     
     # Define global count variables
     self.a_count = 0
@@ -18,6 +43,8 @@ class encoder:
     self.CFG_Encoder_Int()
 
   def CFG_Encoder_Int(self):
+    self.logger.info('Initializing pin directions and enabling interrupts on specified channels')
+
     #set up gpio module
     GPIO.setmode(GPIO.BCM)
   
@@ -42,7 +69,10 @@ class encoder:
 
 
   def get_degrees(self):
-    return (self.a_count)*360/self.ppr
+    deg = (self.a_count)*360/self.ppr
+    self.logger.debug('Shaft encoder degrees: {}'.format(deg))
+    return deg
   
   def get_count(self):
+    self.logger.debug('Shaft encoder count: {}'.format(self.a_count))
     return self.a_count
