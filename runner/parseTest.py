bc2cb3fd8b99e94e486b9367d5c36a142c063719
Glen Nicholls <dkillers303@gmail.com>
2018-11-16 22:04:15 -0700

    updated parser test and still deciding how it should generate key/val

--- runner/parseTest.py
+++ runner/parseTest.py
@@ -1,24 +1,28 @@
 import os
-import csv
+import itertools
 
-def read_file(filename):
+  
+def parse_gpio_file(filename):
   if not os.path.exists(filename):
-    raise ValueError('file {} does not exist'.format(filename))
-
+    raise ValueError('file {} does not exist in {}'.format(filename, os.getcwd()))
+  
+  line_num  = 0
+  dict_keys = []
+  dict_vals = []
   with open(filename, mode='r') as fp:
-    file = csv.reader(fp)
-  return(file)
-  
-def parse_gpio_file(filename)
-  if not os.path.exists(filename):
-    raise ValueError('file {} does not exist'.format(filename))
-  line_num = 0
-  
-  with open(filename, mode='r') as fp
-    for line in fp.readlines():
-      if line_num == 0:
+    lines = (line.rstrip() for line in fp)
+    lines = [line for line in lines if line]
+    for line in lines:
+      #print(line)
+      if line.startswith('#'):
         pass
-      elif line.startswith
+      elif line_num == 0:
+        dict_keys.append( line.strip(',') )
+      else:
+        dict_vals.append( [x.lstrip() for x in line.split(',')] )
+      line_num += 1
+  print(dict_keys)
+  print(dict_vals)
   
 
-read_file('gpio.csv')
+parse_gpio_file('gpio.csv')
