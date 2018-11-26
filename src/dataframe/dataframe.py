ecfcf67863ef3599015a0e9454852390017416e6
Glen Nicholls <dkillers303@gmail.com>
2018-11-25 19:17:12 -0700

    updated dataframe

--- src/dataframe/dataframe.py
+++ src/dataframe/dataframe.py
@@ -1,7 +1,8 @@
+import logging
 import pandas as pd
 import pickle
 
-class df:
+class dataframe:
   
   '''
     create dataframe with given columnn names
@@ -9,42 +10,75 @@ class df:
     columns: array of column names
     fileLoc: string file location for dumps and restores
   '''
-  def __init__(self, fromPickle, columns, fileLoc):
-    self.fileLoc = fileLoc
-    if fromPickle == True:
-      self.frame = pd.read_pickle(fileLoc)
-    else:
-      self.frame = pd.DataFrame(columns=columns)
+  def __init__(self, logger_name        = 'main_logger', 
+                     logger_module_name = 'dataframe',
+                     columns            = None, 
+                     file_location      = ''
+               ):
+    self.logger = logging.getLogger(logger_name + '.' + logger_module_name)
+    self.logger.info('creating an instance of the ' + __name__ + ' with the alias {}'.format(logger_module_name))
+
+    self.pd = pd
+
+    # file location
+    self.fileLoc = file_location
+    if self.fileLoc == '':
+      raise ValueError('Invalid file location, must be valid string')
+    self.pickle = self.pd.read_pickle(self.fileLoc)
+    print('pickle: {}'.format(self.pickle))
+
+    # dataframe
+    self.column_names = columns
+    if self.column_names == None:
+      raise ValueError('Invalud column names, must be list of strings: [\'str1\', \'str2\',...]')
+    self.frame = self.pd.DataFrame(columns=columns)
+    print('frame: {}'.format(self.frame))
 
 
   '''
     return array of columns names
   '''
-  def getColumnNames(self):
-    return self.frame.columns.valuess
+  def get_keys(self):
+    column_names = self.frame.columns.values
+    self.logger.debug('Column Names: {}'.format(column_names))
+    return column_names
 
   
   '''
     push row data into df
     row: dict with keys that match frame column names
   '''
-  def addRow(self, row):
-    self.frame = self.frame.append(row, ignore_index=True)
-    print(self.frame.head())
+  def set_row(self, row):
+    self.pickle = self.pickle.append(row, ignore_index=True)
+    new_row = self.pickle.head()
+    self.logger.debug('Added row: {}'.format(row))
 
+
+  '''
+    pop row data into df
+    row: dict with keys that match frame column names
+  '''
+  def get_row(self, row=-1):
+    tail = self.pickle.tail(row)
+    self.logger.info('Popped row {} index from the end: {}'.format(row, tail))
+    return tail
+    
   
   '''
     change fileLoc
   '''
-  def updateFileLoc(self, newFileLoc):
+  def set_file_location(self, newFileLoc):
+    self.logger.debug('Changing pickle file location to: {}'.format(newFileLoc))
     self.fileLoc = newFileLoc
+    self.pickle  = pd.read_pickle(self.fileLoc)
 
   
   '''
     dump frame to .pkl file at fileLoc location
   ''' 
   def dump(self):
-    self.frame.to_pickle(self.fileLoc)
+    self.logger.debug('Dumping dataframe to: {}'.format(self.fileLoc))
+    self.pickle.to_pickle(self.fileLoc)
 
 
 
@@ -64,9 +98,8 @@ columns = [ "time",
 
 fileLoc = './test.pkl'
 
-frame = df( fromPickle = False,
-            columns = columns,
-            fileLoc = fileLoc)
+frame = dataframe( columns = columns,
+            file_location = fileLoc)
 
 # build dict with positive intgers
 row1 = {}
@@ -74,8 +107,8 @@ for i, col in enumerate(columns):
   row1[col] = i
 
 # append row1 to frame
-frame.addRow(row1)
-print(frame.frame.head())
+frame.set_row(row1)
+print(row1)
 
 #build dict with negative integers
 row2 = {}
@@ -83,22 +116,29 @@ for i, col in enumerate(columns):
   row2[col] = -i
 
 #append row2 to frame and print
-frame.addRow(row2)
+frame.set_row(row2)
 print('frame:')
-print(frame.frame.head())
+print(frame.pickle)
 
 #dump frame to file
 frame.dump()
+
+# get newest entry
+tail = frame.get_row(10)
+print('frame:\n{}'.format(tail))
 
 # clear frame
 frame = None
 print('frame:')
 print(frame)
 
-# load new frame from file
-frame2 = df(  fromPickle = True,
-              columns = None,
-              fileLoc = fileLoc)
 
-print('frame2:')
-print(frame2.frame.head())
+# # load new frame from file
+# frame2 = dataframe( columns = None,
+#               file_location = fileLoc)
+# 
+# print('frame2:')
+# print(frame2.frame[-1])
+# 
+# print('raw frame2:')
+# print(frame2.get_keys())
