1161982daabac6be05363bc90ccf89c46bd9478c
Glen Nicholls <gnicholl@uccs.edu>
2018-11-30 10:30:43 -0700

    bug fix in utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -178,7 +178,7 @@ class dataframe:
     '''
       dump frame to .csv file at fileLoc location
     ''' 
-    def dump_pickle(self):
+    def dump_csv(self):
         self.logger.info('Dumping dataframe to: {}'.format(self._file_loc))
         self._frame.to_csv(self._file_loc, encoding='utf-8', index=False) #false index so we aren't writing index
 
