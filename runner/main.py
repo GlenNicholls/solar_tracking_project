bc6af13378c31e9def555501ba6b2a784b54f71b
bradhuber1 <bradhuber1@yahoo.com>
2018-12-01 15:12:54 -0700

    undo of bypass of shaft encoders from last commit

--- runner/main.py
+++ runner/main.py
@@ -655,8 +655,6 @@ def menu_normal_op():
   
   #Get astral with current location
   loc_astral = get_location_astral(GLOB.latitude, GLOB.longitude, GLOB.elevation)
-  
-  last_solar_deg_az, last_solar_deg_el = get_solar_position_deg(loc_astral)
        
   # infinite loop
   while True:
@@ -675,10 +673,8 @@ def menu_normal_op():
       solar_deg_az, solar_deg_el = get_solar_position_deg(loc_astral)
       
       # Move to calculated sun posistion
-      #deg_az = solar_deg_az - prev_enc_az
-      #deg_el = solar_deg_el - prev_enc_el 
-      deg_az = solar_deg_az - last_solar_deg_az
-      deg_el = solar_deg_el - last_solar_deg_el
+      deg_az = solar_deg_az - prev_enc_az
+      deg_el = solar_deg_el - prev_enc_el 
 	  
       if deg_az > 1.0 or deg_el > 1.0:
         open_loop_locked = move_motors(deg_az, deg_el, open_loop=True)
@@ -707,10 +703,8 @@ def menu_normal_op():
       # Move to sunrise position for tomorrow
       logger.info('Moving to sunrise position for tomorrow')
       
-      #deg_az = solar_deg_az - prev_enc_az
-      #deg_el = solar_deg_el - prev_enc_el 
-      deg_az = solar_deg_az - last_solar_deg_az
-      deg_el = solar_deg_el - last_solar_deg_el
+      deg_az = solar_deg_az - prev_enc_az
+      deg_el = solar_deg_el - prev_enc_el 
       open_loop_locked = move_motors(deg_az, deg_el, open_loop=True)
     
 	# log system parameters
@@ -718,9 +712,6 @@ def menu_normal_op():
     df_logger.append_row(GLOB.df_dict)
     df_logger.dump_pickle() # dump to pickle file
     df_logger.dump_csv()    # dump to csv file for plotting
-	
-    last_solar_deg_az = deg_az
-    last_solar_deg_el = deg_el
 	
     logger.warn('Sleep calculation NOT DEFINED, defaulting sleep to 30s in loop')
     time.sleep(30)
