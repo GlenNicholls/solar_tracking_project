cc84c7bb9f6e53eef549ff85e839170c0e33cb0c
Glen Nicholls <gnicholl@uccs.edu>
2018-12-06 18:44:41 -0700

    debug in encoder

--- src/shaft_encoder/encoder.py
+++ src/shaft_encoder/encoder.py
@@ -15,7 +15,8 @@ class encoder:
     # instantiate logger
     self.logger = logging.getLogger(logger_name + '.' + logger_module_name)
     self.logger.info('creating an instance of the ' + __name__ + ' with the alias {}'.format(logger_module_name))
-
+    print(GPIO)
+    
     # Define pin numbers for A/B channels on azimuth/elevation motors
     self.A_pin=a_pin
     self.B_pin=b_pin
